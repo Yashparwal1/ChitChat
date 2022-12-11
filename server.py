96fd0aca9ce5ffa26fc972abbf002de97203ec9b
Yash Parwal <yashparwal1@gmail.com>
2022-12-11 23:37:16 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,27 +3,36 @@ import socket
 import threading
 
 #thread recieve function
-def thread_recv(s,signal):
+def thread_recv(sock_fd):
   while True:
-      tmp_recv = s.recv(1024)
+      tmp_recv = sock_fd.recv(1024)
       print("\n[+] Data:", tmp_recv.strip().decode())
       if(tmp_recv.strip() == b'exit'):
         print("[!] byebye!")
-        s.close()
+        sock_fd.close()
         break
 
-#main function for establishing connection and sending data
+#main function for listening for connection from client and sending data
 def main():
   HOST = "127.0.0.1" #IP address
-  PORT = 1234 #Port no. to connect with
+  PORT = 1234 #Port no. to listen on
   s = socket.socket()
-  s.connect((HOST,PORT))
-  print("[+] Connection established...")
-  new_thread = threading.Thread(target=thread_recv, args=(s,True)) #calling thread_recv function as a thread
-  new_thread.start() # run the thread
-  print("[+] recv is running as a thread!")
-  while True:
-    tmp_send = input("[+] input: ").encode()
-    s.send(tmp_send+b'\n')
+  s.bind((HOST, PORT))
+  s.listen(1)
+  print("[+] Listining...")
+  sock_fd, conn = s.accept()
+  if(conn):
+    print("[+] new connection:", conn)
+    new_thread = threading.Thread(target=thread_recv, args=[sock_fd,])
+    new_thread.start() # run the thread
+    print("[+] recv is running as a thread!")
+    while True:
+      tmp_recv = sock_fd.recv(1024)
+      print("\n[+] Data:", tmp_recv.strip().decode())
+      if(tmp_recv.strip() == b'exit'):
+        print("[!] byebye!")
+        sock_fd.close()
+        break
 
-main() #calling main function
+
+main()
