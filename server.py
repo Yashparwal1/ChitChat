a1c326cb7921499e92432f2f74148c03bc25df7e
Yash Parwal <yashparwal1@gmail.com>
2022-12-18 02:16:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,19 +2,38 @@
 import socket
 import threading
 
+#function for encryption
+def enc_xor(data,key):
+  res = b''
+  for each_chr in data:
+    temp = ord(each_chr)^key
+    res = res + chr(temp).encode()
+  return res
+
+#function for decryption
+def dec_xor(data,key):
+  res = b''
+  for each_chr in data:
+    temp = each_chr^key
+    res = res + chr(temp).encode()
+  return res 
+
+
 #thread recieve function
-def thread_recv(sock_fd):
+def thread_recv(sock_fd,key):
   while True:
-      tmp_recv = sock_fd.recv(1024)
-      print("\n[+] Data:", tmp_recv.strip().decode())
-      if(tmp_recv.strip() == b'exit'):
+      encrypted_msg = sock_fd.recv(1024)
+      print(encrypted_msg)
+      dec_msg = dec_xor(encrypted_msg,key) #calling decrytion function
+      print("\n[+] Data:", dec_msg.strip().decode())
+      if(dec_msg.strip() == b'exit'):
         print("[!] byebye!")
         sock_fd.close()
         break
 
 #main function for listening for connection from client and sending data
 def main():
-  HOST = "127.0.0.1" #IP address
+  HOST = "0.0.0.0" #IP address
   PORT = 1234 #Port no. to listen on
   s = socket.socket()
   s.bind((HOST, PORT))
@@ -23,16 +42,13 @@ def main():
   sock_fd, conn = s.accept()
   if(conn):
     print("[+] new connection:", conn)
-    new_thread = threading.Thread(target=thread_recv, args=[sock_fd,])
+    key = 25 #the key used in the XOR encryption
+    new_thread = threading.Thread(target=thread_recv, args=(sock_fd,key))
     new_thread.start() # run the thread
     print("[+] recv is running as a thread!")
     while True:
-      tmp_recv = sock_fd.recv(1024)
-      print("\n[+] Data:", tmp_recv.strip().decode())
-      if(tmp_recv.strip() == b'exit'):
-        print("[!] byebye!")
-        sock_fd.close()
-        break
-
+      normal_msg = input("[+] input: ")
+      enc_msg = enc_xor(normal_msg,key) #calling encrytion function
+      sock_fd.send(enc_msg+b'\n')
 
 main()
